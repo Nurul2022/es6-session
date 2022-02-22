9214132b0cfb24e643195281595aa1afe2a2e4c2
Nurul Islam <nurulbdpersonal@gmail.com>
2022-02-22 13:56:17 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+//1. What is Es6 & why we should use it? 
+/*
+//2. let 
+var myyName = 'Programming Hero';
+let myName = 'Programming Hero';
+//let myName = 'Sadik';
+//  -- Redeclare kora jabena.
+//  -- Declare korar age define kora jabena
+//  -- Block scope feature
+
+ let x = 10
+
+{
+  let x = 2
+console.log('Inside block', x)
+ }
+console.log('Outside block', x) 
+*/
+/*
+3. //const
+//-- cannot reassign
+//-- has other features of let
+const age = 45
+//age = 56
+console.log(age)  */
+
+/*
+4. //Template Literal
+let name = 'Nazrul'
+let message = 'How are you?'
+//Old way
+//let fullMessage = 'Hello ' + name + ', ' + message
+
+let fullMessage = `Hi, ${9 + 9}, ${message}`
+
+console.log(fullMessage)
+*/
